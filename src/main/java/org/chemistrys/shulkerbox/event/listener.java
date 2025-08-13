529a481b6c9260a4868055d43e0b5efe859a012f
chemistrys <13315020392@163.com>
2025-08-14 00:41:44 +0800

    commit

--- src/main/java/org/chemistrys/shulkerbox/event/listener.java
+++ src/main/java/org/chemistrys/shulkerbox/event/listener.java
@@ -1,4 +1,17 @@
 package org.chemistrys.shulkerbox.event;
 
-public class listener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.*;
+
+public class listener implements Listener {
+
+    @EventHandler
+    public void PlayerJoin(PlayerJoinEvent e)
+    {
+    }
+    public PlayerAnimationEvent PlayerAnimationEvent(PlayerAnimationEvent e)
+    {
+        return e;
+    }
 }
